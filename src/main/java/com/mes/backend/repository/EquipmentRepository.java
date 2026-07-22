cc90cc468a7a1c7302b173db0b9b318bfda186b7
pyyzzz <kooor20089@gmail.com>
2026-07-22 14:42:47 +0900

    DataSeeder.java 확장 PROCESS, EQUIPMENT, DEFECT_TYPE, MEASUREMENT_SPEC 4종 시드 데이터 추가

--- src/main/java/com/mes/backend/repository/EquipmentRepository.java
+++ src/main/java/com/mes/backend/repository/EquipmentRepository.java
@@ -1,9 +1,12 @@
 // Equipment(설비) 레포지토리
 package com.mes.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mes.backend.entity.Equipment;
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+    Optional<Equipment> findByEquipmentCode(String equipmentCode);
 }
